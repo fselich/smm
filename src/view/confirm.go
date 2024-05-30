2d31216d9fed7c08007db146e24d8ce8951c4392
Felipe <felipe.sanchez@elich.es>
2024-05-30 09:24:16 +0200

    Modal

---
+++ src/view/confirm.go
@@ -0,0 +1,34 @@
+package view
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/erikgeiser/promptkit/confirmation"
+)
+
+type Confirm struct {
+	question     string
+	message      any
+	confirmation *confirmation.Model
+}
+
+func NewConfirm(question string, message any) *Confirm {
+	confirm := confirmation.New("Are you ready?", confirmation.Yes)
+	confirm.Template = confirmation.TemplateArrow
+	confirm.ResultTemplate = confirmation.ResultTemplateArrow
+
+	confirmModel := confirmation.NewModel(confirm)
+	return &Confirm{question: question, message: message, confirmation: confirmModel}
+}
+
+func (c *Confirm) View() string {
+	return c.confirmation.View()
+}
+
+func (c *Confirm) Init() tea.Cmd {
+	return c.confirmation.Init()
+}
+
+func (c *Confirm) Update(msg tea.Msg) (*Confirm, tea.Cmd) {
+	_, cmd := c.confirmation.Update(msg)
+	return c, cmd
+}
