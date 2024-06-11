aa33675f29b878742b34d4b8f29bb48ba1db2e4b
felipe <felipe.sanchez@elich.es>
2024-06-11 16:11:06 +0200

    remove data

--- src/bootstrap/bootstrap.go
+++ src/bootstrap/bootstrap.go
@@ -21,7 +21,7 @@ func LoadConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(configPath)
-	viper.SetDefault("projectIds", []string{"civitatis-guias-1", "otra cosa"})
+	viper.SetDefault("projectIds", []string{"project1", "project2"})
 
 	if _, err := os.Stat(configFile); os.IsNotExist(err) {
 		err = viper.WriteConfigAs(configFile)
